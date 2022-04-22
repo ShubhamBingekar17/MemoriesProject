f44ab110390bf82ae01584489b74bfb10a09243e
Shubham Bingekar <75237471+ShubhamBingekar17@users.noreply.github.com>
2022-04-22 22:28:03 +0530

    commit to main

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import bodyParser from 'body-parser';
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
@@ -12,9 +13,12 @@ app.use(cors());
 
 app.use('/posts' , postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://hexadevelopers:teamhexa123@cluster0.j3zox.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+app.use('/' , (req, res) => {
+    res.send('Hello to Memories APi');
+})
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL , { useNewUrlParser: true , useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL , { useNewUrlParser: true , useUnifiedTopology: true})
     .then(() => app.listen(PORT , () => console.log(`Server running on PORT : ${PORT}`)))
     .catch((error) => console.log(error.message));
